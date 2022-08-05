51496ce01271f85d3b9ff67e00a2513e3bab1088
cbemben <cbemben04@gmail.com>
2022-08-05 11:17:52 -0400

    diff materialization and shifting to stg objects

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -1,30 +1,15 @@
 {{
     config(
-        materialized='view'
+        materialized='table'
         )
 }}
 
 with customers as (
-
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from `dbt-tutorial`.jaffle_shop.customers
-
+    select * from {{ ref('stg_customers') }}
 ),
 
 orders as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from `dbt-tutorial`.jaffle_shop.orders
-
+    select * from {{ ref('stg_orders') }}
 ),
 
 customer_orders as (
@@ -41,7 +26,6 @@ customer_orders as (
     group by 1
 
 ),
-
 
 final as (
 
